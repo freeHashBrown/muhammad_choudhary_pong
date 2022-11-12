b27abb2e18ec09bd6a983df18cbc74845201bdd6
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-11-12 15:52:58 -0500

    Create helper function to set and get x and y values

--- Ball.js
+++ Ball.js
@@ -2,4 +2,26 @@ export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem
     }
+
+    //Helper function to get x values
+    get x() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
+    }
+    set x(value) {
+        this.ballElem.style.setProperty("--x", value);
+    }
+
+    //Helper function to get y values
+    get y() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"));
+    }
+    set y(value) {
+        this.ballElem.style.setProperty("--y", value);
+    }
+
+    //Update function that will be passed delta
+    update(delta) {
+        this.x = 5;
+        this.y = 15;
+    }
 }
